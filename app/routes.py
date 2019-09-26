dadd7dbcf6e60e21c4a72e009a3ab1394e815d45
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2019-09-26 11:01:41 +1000

    add ligin form

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.jinja', title='Главная')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.jinja', title='Авторизация', form=form)
+
+
+@app.route('/addtrack')
+def add_track():
+    return render_template('addtrack.jinja')
