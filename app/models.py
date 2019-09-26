3f103f6986f7d27bc95bc86ce9511a787cdbdebb
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2019-09-26 13:14:24 +1000

    add a track model

--- app/models.py
+++ app/models.py
@@ -1,11 +1,27 @@
+from datetime import datetime
 from app import db
 
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    tracks = db.relationship('Track', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Track(db.Model):
+    __tablename__ = 'tracks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<Track {}>'.format(self.title)
